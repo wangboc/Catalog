2d44f69a59937c34808fd7dcd7b7435d86467ec3
wangboc <447537045@qq.com>
2015-04-28 21:27:06 +0800

    null

--- QuickCatalog/static/QuickCatalog/JS/player.js
+++ QuickCatalog/static/QuickCatalog/JS/player.js
@@ -90,6 +90,22 @@ $(document).ready(function () {
 });
 
 
+$(function () {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon glyphicon-plus').removeClass('glyphicon glyphicon-minus');
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon glyphicon-minus').removeClass('glyphicon glyphicon-plus');
+        }
+        e.stopPropagation();
+    });
+});
+
+
 // initialize video.js
 var player = videojs("player");
 
