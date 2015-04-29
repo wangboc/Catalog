2aa3941f09eec5a36464cd44d992189684c20bbb
hoh <447537045@qq.com>
2015-04-29 13:05:08 +0800

    实验室数据库修改

--- Catalog/settings.py
+++ Catalog/settings.py
@@ -76,16 +76,28 @@ WSGI_APPLICATION = 'Catalog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'NAME': 'MAM-LINE',
+#         'ENGINE': 'sqlserver_ado',
+#         'HOST': 'localhost',
+#         'USER': 'sa',
+#         'PASSWORD': 'wangwang123',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'NAME': 'MAM-LINE',
         'ENGINE': 'sqlserver_ado',
-        'HOST': 'localhost',
+        'HOST': '10.1.70.88',
         'USER': 'sa',
         'PASSWORD': 'wangwang123',
+        # 'OPTIONS':{
+        #     'provider': 'SQLNCLI.1',
+        # }
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
