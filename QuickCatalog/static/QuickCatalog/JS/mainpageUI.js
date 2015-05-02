a3b91f699981fa8ee16a957edb9395d51c550187
wangboc <447537045@qq.com>
2015-05-02 23:32:01 +0800

    添加knockoutjs框架

--- QuickCatalog/static/QuickCatalog/JS/mainpageUI.js
+++ QuickCatalog/static/QuickCatalog/JS/mainpageUI.js
@@ -1,3 +1,16 @@
 /**
  * Created by ho on 2015/5/2.
  */
+
+
+
+$(document).ready(function () {
+    $("tr").dblclick(function () {
+        $("#programsTab").load("/quickcatalog/23031/programinfo/", function (responseTxt, statusTxt, xhr) {
+            if (statusTxt == "success")
+                alert("外部内容加载成功！");
+            if (statusTxt == "error")
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+        });
+    });
+});
